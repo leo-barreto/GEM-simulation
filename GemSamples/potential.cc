b9a2a163e586084c7af09bb57a10d362c7111557
MediocrePhysicist <lboc.campos@gmail.com>
2018-03-19 21:21:37 -0300

    Added tests for differents potentials

---
+++ GemSamples/potential.cc
@@ -0,0 +1,19 @@
+#include "../GemSampa.hh"
+#include <string>
+#include <fstream>
+
+
+using namespace std;
+
+int main(int argc, char * argv[]) {
+
+  double g[9];
+  float potent = atof(argv[1]);
+  string T = argv[1];
+  string S = "gem70_140_" + T;
+  string TXT = "t" + T + ".txt";
+
+  SetupInfo(g, S, 70., 140., 0.1, 0.1, 0.005, 0.0005, 3000., 1000., potent);
+  GainOneElectron(S, g, TXT, false, 0, 1, 0.05, true);
+
+}
